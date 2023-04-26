cd369d558d27ff2e628504ce4ae9d596ee7e309a
Haris-kaispe <haris.khan@kaispe.com>
2023-04-26 18:38:59 +0500

    Removed files ignored by .gitignore

--- Config.js
+++
@@ -1,6 +0,0 @@
-export const config = {
-  appId: import.meta.env.VITE_APP_ID || "",
-  redirectUri: import.meta.env.VITE_REDIRECT_URI || "",
-  scopes: ["user.read"],
-  authority: "https://login.microsoftonline.com/common"
-};
